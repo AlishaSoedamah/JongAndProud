53e3d4d5722767057ee3edabb43d03f75f8ddd30
alisha <Alisha.soedamah@gmail.com>
2023-01-17 21:55:47 +0100

    WIP: redo hero

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Home Jong & Proud 🏳️‍🌈</title>
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css" >
+        <link href="{{ asset('css/header.css') }}" rel="stylesheet" type="text/css" >
         <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
     </head>
     <body>
