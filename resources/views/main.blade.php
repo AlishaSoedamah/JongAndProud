df7231656579bd441c8b55973b93df39ab653e1a
alisha <Alisha.soedamah@gmail.com>
2023-04-10 19:39:12 +0200

    Idk anymore

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Home 🏳️‍🌈 🏳️‍⚧️ | Jong & Proud Amersfoort</title>
-        <link href="{{ asset('/css/styles.css') }}" rel="stylesheet" type="text/css" >
-        <link href="{{ asset('/css/header.css') }}" rel="stylesheet" type="text/css" >
-        <script type="module" src="{{ URL::asset('/scripts/animations.js') }}"></script>
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css" >
+        <link href="{{ asset('css/header.css') }}" rel="stylesheet" type="text/css" >
+        <script type="module" src="{{ URL::asset('scripts/animations.js') }}"></script>
         <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.png') }}">
     </head>
     <body>
