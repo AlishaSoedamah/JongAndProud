1c7cd62e744d2ce9ba1d843eda98a9513c643b71
Alpakash <akash.soedamah@gmail.com>
2023-04-12 19:52:57 +0200

    create outputDirectory dist in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist',
+    },
     plugins: [
         laravel({
             input: [
