103036c0780a80893ff92cb1d2cd0058f7c71cd9
Alpakash <akash.soedamah@gmail.com>
2023-04-12 21:19:01 +0200

    Dunno what went wrong.

--- api/assets.php
+++ api/assets.php
@@ -14,25 +14,25 @@ if ($_GET['type'] === 'css') {
 
 if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/scripts/' . basename($_GET['file']);
+    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
 }
 
-// if ($_GET['type'] === 'jpeg') {
-//     header('Content-Type: image/jpeg');
-//     echo require __DIR__ . imagesRoute . basename($_GET['file']);
-// }
+if ($_GET['type'] === 'jpeg') {
+    header('Content-Type: image/jpeg');
+    echo require __DIR__ . imagesRoute . basename($_GET['file']);
+}
 
-// if ($_GET['type'] === 'webp') {
-//     header('Content-Type: image/png');
-//     echo require __DIR__ . imagesRoute . basename($_GET['file']);
-// }
+if ($_GET['type'] === 'webp') {
+    header('Content-Type: image/png');
+    echo require __DIR__ . imagesRoute . basename($_GET['file']);
+}
 
-// if ($_GET['type'] === 'jpeg') {
-//     header('Content-Type: image/webp');
-//     echo require __DIR__ . imagesRoute . basename($_GET['file']);
-// }
+if ($_GET['type'] === 'jpeg') {
+    header('Content-Type: image/webp');
+    echo require __DIR__ . imagesRoute . basename($_GET['file']);
+}
 
-// if ($_GET['type'] === 'ico') {
-//     header('Content-Type: image/x-icon');
-//     echo require __DIR__ . '/../public/' . basename($_GET['file']);
-// }
+if ($_GET['type'] === 'ico') {
+    header('Content-Type: image/x-icon');
+    echo require __DIR__ . '/../public/' . basename($_GET['file']);
+}
