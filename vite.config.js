c292a5d1e8cae7bf59b8ed01758fab8d4cce3bb8
Alpakash <akash.soedamah@gmail.com>
2023-04-12 20:18:29 +0200

    add dist to public

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-    },
     plugins: [
         laravel({
             input: [
