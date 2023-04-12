8f8b703132e7bd3a0329264ec9e5d377939547b7
Alpakash <akash.soedamah@gmail.com>
2023-04-12 21:12:33 +0200

    Did I make a typo when changing all the blade files?

--- api/assets.php
+++ api/assets.php
@@ -4,24 +4,35 @@
  * Built assets aren't currently routeable via vercel-php
  * Manually route assets to be found
  */
+
+$imagesRoute = '/../public/images/';
+
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
     echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
-} else if ($_GET['type'] === 'js') {
+}
+
+if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
     echo require __DIR__ . '/../public/scripts/' . basename($_GET['file']);
-} 
+}
 
-else if ($_GET['type'] === 'jpeg') {
+if ($_GET['type'] === 'jpeg') {
     header('Content-Type: image/jpeg');
-    echo require __DIR__ . '/../public/images/' . basename($_GET['file']);
-} else if ($_GET['type'] === 'webp') {
+    echo require __DIR__ . imagesRoute . basename($_GET['file']);
+}
+
+if ($_GET['type'] === 'webp') {
     header('Content-Type: image/png');
-    echo require __DIR__ . '/../public/images/' . basename($_GET['file']);
-} else if ($_GET['type'] === 'jpeg') {
+    echo require __DIR__ . imagesRoute . basename($_GET['file']);
+}
+
+if ($_GET['type'] === 'jpeg') {
     header('Content-Type: image/webp');
-    echo require __DIR__ . '/../public/images/' . basename($_GET['file']);
-} else if ($_GET['type'] === 'ico') {
+    echo require __DIR__ . imagesRoute . basename($_GET['file']);
+}
+
+if ($_GET['type'] === 'ico') {
     header('Content-Type: image/x-icon');
     echo require __DIR__ . '/../public/' . basename($_GET['file']);
 }
